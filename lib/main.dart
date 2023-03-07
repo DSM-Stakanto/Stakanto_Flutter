0fd880936d5824b7f507d47a5227ed9a8f4a5128
minseo-and <msp05070478@gmail.com>
2023-03-07 10:20:03 +0900

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:stakanto/screen/home_screen.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
+    return MaterialApp(
+      home: const HomeScreen(),
+    );
+  }
+}
